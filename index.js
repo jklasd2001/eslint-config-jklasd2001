4e5c08253f8e45582bfcd0a3b65ba8c9fe748cb0
김영수 주임 연구원 <jklasd2001@musma.net>
2022-08-31 22:44:56 +0900

    feat: initialize

---
+++ index.js
@@ -0,0 +1,80 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:prettier/recommended',
+    ],
+    plugins: ['unused-imports'],
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        },
+        typescript: {},
+      },
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // import React from 'react' 제외
+      'import/default': 'off',
+      'import/order': [
+        // Import Sort
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'type',
+            'unknown',
+            'object',
+          ],
+          pathGroups: [
+            {
+              pattern: '{react*, react*/**}',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'src/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/no-unresolved': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-named-as-default': 'off',
+      'eol-last': 'error',
+      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 100,
+          semi: false,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+        },
+      ],
+    },
+  }
