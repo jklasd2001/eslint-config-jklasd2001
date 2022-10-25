2bd6f4456f0fe01869c7bbf0890840c25fa8b43c
jklasd2001 <jklasd2001@naver.com>
2022-10-25 19:33:23 +0900

    feat: 의존성 버전 업데이트

---
+++ README.md
@@ -0,0 +1,3 @@
+## eslint-config-jklasd2001
+
+React 관련 프로젝트 진행 시 사용할 공통 ESLint Config입니다.
--- index.js
+++ index.js
@@ -1,80 +1,82 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:import/recommended',
-      'plugin:import/typescript',
-      'plugin:prettier/recommended',
-    ],
-    plugins: ['unused-imports'],
-    settings: {
-      'import/resolver': {
-        node: {
-          extensions: ['.ts', '.tsx', '.js', '.jsx'],
-        },
-        typescript: {},
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['unused-imports'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
       },
-      react: {
-        version: 'detect',
-      },
+      typescript: {},
     },
-    rules: {
-      'react/react-in-jsx-scope': 'off', // import React from 'react' 제외
-      'import/default': 'off',
-      'import/order': [
-        // Import Sort
-        'error',
-        {
-          groups: [
-            'builtin',
-            'external',
-            'internal',
-            ['parent', 'sibling', 'index'],
-            'type',
-            'unknown',
-            'object',
-          ],
-          pathGroups: [
-            {
-              pattern: '{react*, react*/**}',
-              group: 'external',
-              position: 'before',
-            },
-            {
-              pattern: 'src/**',
-              group: 'internal',
-            },
-          ],
-          pathGroupsExcludedImportTypes: ['react'],
-          'newlines-between': 'always',
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // import React from 'react' 제외
+    'import/default': 'off',
+    'import/order': [
+      // Import Sort
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'type',
+          'unknown',
+          'object',
+        ],
+        pathGroups: [
+          {
+            pattern: '{react*, react*/**}',
+            group: 'external',
+            position: 'before',
           },
+          {
+            pattern: 'src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
-      ],
-      'import/no-unresolved': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-named-as-default': 'off',
-      'eol-last': 'error',
-      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-      ],
-      'prettier/prettier': [
-        'error',
-        {
-          printWidth: 100,
-          semi: false,
-          singleQuote: true,
-          tabWidth: 2,
-          trailingComma: 'all',
-        },
-      ],
-    },
-  }
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'eol-last': 'error',
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+      },
+    ],
+  },
+}
